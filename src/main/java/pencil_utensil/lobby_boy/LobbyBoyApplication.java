6b6818668c35d6a2c24ba501945dcd613aa9a7ce
alldaygooning <nikich1402@mail.ru>
2025-09-27 14:35:47 +0300

    Initial commit

---
+++ src/main/java/pencil_utensil/lobby_boy/LobbyBoyApplication.java
@@ -0,0 +1,13 @@
+package pencil_utensil.lobby_boy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LobbyBoyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LobbyBoyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pencil_utensil/lobby_boy/LobbyBoyApplicationTests.java
@@ -0,0 +1,13 @@
+package pencil_utensil.lobby_boy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LobbyBoyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
